b7bd8d4a0616f952515a2719d9a3f6453e02dc19
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-11 11:44:50 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,49 +1,29 @@
 import products from "./data/products.json";
-import { Cart, cartProduct, Product } from "./interfaces";
-import Rxjs, { Observable } from "rxjs";
+import { Product } from "./interfaces";
+import { from } from "rxjs";
 
-/* const addProduct = (
-  cart: Cart,
-  productName: string,
-  productDetails: cartProduct
-) => {
-  cart[productName] = productDetails;
-  //can be immutable
-}; */
+let cart = from(products);
 
-const removeProduct = (
-  cart: Observable<Product>,
-  productNameToRemove: string
-) => delete cart[productNameToRemove];
+const addProduct = (product: Product) => products.push(product as any);
 
-const updateProductAmount = (
-  cart: Observable<Product>,
-  productName: string,
-  amount: number
-) =>
-  (amount > 0 && !cart[productName].limit) ||
-  (amount <= cart[productName].limit && (cart[productName].amount = amount));
+const removeProduct = (product: Product) =>
+  products.filter((prdct) => prdct.name !== product.name);
 
-/* const checkout = (cart: Observable<Product>) => {
-  cart = {};
-}; */
+const updateProductAmount = (product: Product, newAmount: number) =>
+  products.forEach((prdct) => {
+    if (prdct.name === product.name) prdct.amount = newAmount;
+  });
 
-const totalPrice = (cart: Observable<Product>): number => {
-  const sum = Object.keys(cart).reduce(
-    (previousPrice: number, currentProductName: string) =>
-      previousPrice +
-      cart[currentProductName].price * cart[currentProductName].amount,
+const checkout = () => (cart = from([]));
+
+const totalPrice = () =>
+  products.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.price,
     0
   );
-  return sum;
-};
 
-const productQuantity = (cart: Observable<Product>) => {
-  return Object.keys(cart).length;
-};
-
-let cart = new Rxjs.Observable((observer) => {
-  products.forEach((product: Product) => observer.next(product));
-});
-
-console.log(totalPrice(cart));
+const productsQuantity = () =>
+  products.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.amount,
+    0
+  );
